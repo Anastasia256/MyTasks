40c520ee6cd5889a16fcee726280af1d7872078b
Anastasia <anastasiadibrova@gmail.com>
2017-07-26 11:24:42 +0300

    implementation Horstman task7

--- src/main/java/task7/Main.java
+++ src/main/java/task7/Main.java
@@ -1,4 +1,40 @@
 package task7;
 
-public class Compare {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Main{
+    public static void main(String[] args) {
+
+        ArrayList<String> stringList = new ArrayList<String>() {{
+            add("C");
+            add("I");
+            add("G");
+            add("H");
+            add("D");
+            add("F");
+            add("A");
+            add("L");
+            add("K");
+            add("J");
+            add("B");
+            add("M");
+            add("E");
+        }} ;
+
+        luckySort(stringList, new StringComparator());
+    }
+
+    public static void luckySort(ArrayList<String> strings, Comparator<String> comp) {
+
+        Collections.sort(strings, comp);
+
+        for (int i = 0; i < strings.size() - 1; i++) {
+            while (strings.get(i).compareTo(strings.get(i + 1)) < 0) {
+                Collections.shuffle(strings);
+            }
+        }
+        System.out.println(strings);
+    }
 }
--- src/main/java/task7/StringComparator.java
+++ src/main/java/task7/StringComparator.java
@@ -1,4 +1,12 @@
 package task7;
 
-public class StringComparator {
+import java.util.Comparator;
+
+public class StringComparator implements Comparator<String> {
+
+    @Override
+    public int compare(String s1, String s2) {
+        return s1.compareTo(s2);
+    }
+
 }
